cf9f54a635ffcb0ebcb2d708c974989fc834bdb7
jayalakshmi <you@example.com>
2019-09-23 19:21:24 +0530

    Added header and rectified the error 500

--- screens/UserList.js
+++ screens/UserList.js
@@ -10,37 +10,24 @@ import {
 } from "react-native";
 import gql from "graphql-tag";
 import { ListItem } from "react-native-elements";
-import { Icon, Header } from "react-native-elements";
+import { Icon } from "react-native-elements";
+import Header from "../util/Header";
 import { Query, withApollo } from "react-apollo";
 const width = Dimensions.get("window").width;
-const GET_USERS_LIST = gql`
-  query usersByBranchID($id: UUID) {
-    usersByBranchID(id: $id) {
-      user {
-        name
-        email
-        phone
-        firstName
-        lastName
-        password
-        address
-        id
-      }
-    }
-  }
-`;
+
 class UserList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      branchId: ""
+      branchId: "",
+      userList: []
     };
     this.delete = this.delete.bind(this);
   }
-  componentDidMount() {
-    AsyncStorage.getItem("branchId").then(value => {
-      this.setState({ branchId: value });
-    });
+  async componentDidMount() {
+    let branchId = await AsyncStorage.getItem("branchId");
+    this.setState({ branchId });
+    this.getUsersByBranch(branchId);
   }
 
   delete = async id => {
@@ -56,99 +43,110 @@ class UserList extends React.Component {
       }
     });
   };
-
+  getUsersByBranch = async id => {
+    console.log(id);
+    let res = await this.props.client.query({
+      query: gql`
+        query usersByBranchID($id: UUID) {
+          usersByBranchID(id: $id) {
+            user {
+              name
+              email
+              phone
+              firstName
+              lastName
+              password
+              address
+              id
+            }
+          }
+        }
+      `,
+      variables: {
+        id: id
+      }
+    });
+    this.setState({ userList: res.data.usersByBranchID });
+  };
   render() {
+    const { userList } = this.state;
     return (
-      <Query query={GET_USERS_LIST} variables={{ id: this.state.branchId }}>
-        {({ loading, error, data }) => {
-          if (loading) return <Text>Loading</Text>;
-          if (error) return <Text>{`Error! ${error.message}`}</Text>;
-          console.log(data);
-          return (
-            <View>
-              <View>
-                <Header
-                  centerComponent={{ text: "Home", style: { color: "#fff" } }}
-                  rightComponent={{ icon: "home", color: "#fff" }}
-                />
-                <Text
-                  style={{
-                    justifyContent: "center",
-                    alignSelf: "center",
-                    padding: 10,
-                    color: "#6699ff",
-                    fontSize: 25
-                  }}
-                >
-                  User List
-                </Text>
-              </View>
-              <ScrollView
-                contentContainerStyle={{
-                  paddingLeft: width / 7
-                }}
-              >
-                {data.usersByBranchID.map((l, i) => (
-                  <ListItem
-                    containerStyle={{
-                      shadowRadius: 5,
-                      shadowColor: "#5c5c5c",
-                      borderWidth: 1,
-                      width: (width * 2) / 3
+      <View>
+        <View>
+          <Header />
+          <Text
+            style={{
+              justifyContent: "center",
+              alignSelf: "center",
+              padding: 10,
+              color: "#6699ff",
+              fontSize: 25
+            }}
+          >
+            User List
+          </Text>
+        </View>
+        <ScrollView
+          contentContainerStyle={{
+            paddingLeft: width / 7
+          }}
+        >
+          {userList.map((l, i) => (
+            <ListItem
+              containerStyle={{
+                shadowRadius: 5,
+                shadowColor: "#5c5c5c",
+                borderWidth: 1,
+                width: (width * 2) / 3
+              }}
+              key={i}
+              title={
+                <Text style={{ paddingBottom: 10 }}>Name: {l.user.name}</Text>
+              }
+              subtitle={
+                <View style={{ flexDirection: "column" }}>
+                  <Text style={{ paddingBottom: 10 }}>
+                    Email: {l.user.email}
+                  </Text>
+                  <Text style={{ paddingBottom: 10 }}>
+                    Phone Number: {l.user.phone}
+                  </Text>
+                  <Text style={{ paddingBottom: 10 }}>Slot: 10:30</Text>
+                  <View
+                    style={{
+                      padding: 2,
+                      justifyContent: "space-between",
+                      flexDirection: "row",
+                      width: width / 15
                     }}
-                    key={i}
-                    title={
-                      <Text style={{ paddingBottom: 10 }}>
-                        Name: {l.user.name}
-                      </Text>
-                    }
-                    subtitle={
-                      <View style={{ flexDirection: "column" }}>
-                        <Text style={{ paddingBottom: 10 }}>
-                          Email: {l.user.email}
-                        </Text>
-                        <Text style={{ paddingBottom: 10 }}>
-                          Phone Number: {l.user.phone}
-                        </Text>
-                        <Text style={{ paddingBottom: 10 }}>Slot: 10:30</Text>
-                        <View
-                          style={{
-                            padding: 2,
-                            justifyContent: "space-between",
-                            flexDirection: "row",
-                            width: width / 15
-                          }}
-                        >
-                          <TouchableOpacity>
-                            <Icon
-                              name="edit"
-                              color="#00ccff"
-                              onPress={() => {
-                                this.props.navigation.navigate("EditForm", {
-                                  rowData: l
-                                });
-                              }}
-                            />
-                          </TouchableOpacity>
-                          <TouchableOpacity>
-                            <Icon
-                              name="delete"
-                              color="red"
-                              onPress={() => {
-                                this.delete(l.user.id);
-                              }}
-                            />
-                          </TouchableOpacity>
-                        </View>
-                      </View>
-                    }
-                  />
-                ))}
-              </ScrollView>
-            </View>
-          );
-        }}
-      </Query>
+                  >
+                    <TouchableOpacity>
+                      <Icon
+                        name="edit"
+                        color="#00ccff"
+                        onPress={() => {
+                          this.props.navigation.navigate("EditForm", {
+                            rowData: l
+                          });
+                        }}
+                      />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                      <Icon
+                        name="delete"
+                        color="red"
+                        onPress={() => {
+                          this.delete(l.user.id);
+                        }}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              }
+            />
+          ))}
+        </ScrollView>
+      </View>
     );
   }
 }
